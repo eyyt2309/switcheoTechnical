73c3dd924987b93a6b5980c714ab051bd92546bc
MrLegumes <ethan97590773@gmail.com>
2025-03-03 17:32:26 +0800

    dunno

---
+++ currencyswapform/src/components/BuySell.tsx
@@ -0,0 +1,12 @@
+function BuySell() {
+  return (
+    <div className="transaction-container">
+      <div className="buy-container">
+        <input type="text"></input>
+      </div>
+      <div className="sell-container"></div>
+    </div>
+  );
+}
+
+export default BuySell;
--- currencyswapform/src/pages/Home.tsx
+++ currencyswapform/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import NavigationBar from "../components/NavigationBar";
-
+import BuySell from "../components/BuySell";
 function Home() {
   return (
     <>
       <NavigationBar />
+      <h3>Trade anytime, anywhere.</h3>
+      <BuySell />
     </>
   );
 }
